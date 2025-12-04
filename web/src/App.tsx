178e75510134840319ed5b0e7d5a2cc7f018cb76
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@vindelicum.eu>
2025-12-04 14:28:59 +0100

    remove logo code

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,7 +6,6 @@ import Awareness from "./routes/Awareness";
 import EventPage from "./routes/EventPage";
 import Home from "./routes/Home";
 import Vis from "./routes/Vis";
-import logo from "./visuals/logo";
 
 interface AppProps {
     readonly className?: string;
@@ -33,7 +32,6 @@ export default class App extends Component<AppProps, AppState> {
                     <Route path="/" component={Home} />
                     <Route path="/events/:name/" component={EventPage} />
                     <Route path="/visuals/" component={Vis} />
-                    <Route path="/logo/" component={logo} />
                     <Route path="/awareness/" component={Awareness} />
                     <Route path="/about/" component={About} />
                 </Router>
